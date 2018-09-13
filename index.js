1d2421308b4b709da2402ca3c1a1535078c55add
Fabiano Amaral <fabiano.amaral@pagar.me>
2018-09-13 09:23:38 -0300

    servidor criado

---
+++ index.js
@@ -0,0 +1,10 @@
+const restify = require('restify')
+
+const server = restify.createServer()
+
+const msgServidorOn = () => {
+  console.log('servidor rodando...')
+}
+
+server.listen(3000, msgServidorOn)
+
